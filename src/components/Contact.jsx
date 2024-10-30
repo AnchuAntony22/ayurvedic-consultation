c474b7b8438f620153545a7dfc934cc799387e25
Anchu Antony <anchuantony2212@gmail.com>
2024-10-30 21:17:51 +0100

    add call and email

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,4 +1,5 @@
-import React, { useRef } from 'react';
+import React from 'react';
+import { useRef } from 'react';
 import { useTranslation } from 'react-i18next';
 import 'swiper/css';
 import 'swiper/css/pagination';
@@ -14,73 +15,68 @@ const Contact = () => {
     const form = formRef.current;
 
     const recipient = 'info@ayurharmonyretreat.se';
-    const subject = t('contact.subject') + ': ' + form.subject.value; // Adjust as necessary
+    const subject = t('contact.subject') + ': ' + form.subject.value;
     const message = `
       Name: ${form.name.value}
       Email: ${form.email.value}
       Subject: ${subject}
       Message: ${form.message.value}
     `;
-    
+
     const encodedMessage = encodeURIComponent(message);
-    window.location.href = `mailto:${recipient}?subject=${subject}&body=${encodedMessage}`;
+    const mailtoLink = `mailto:${recipient}?subject=${subject}&body=${encodedMessage}`;
+
+    window.location.href = mailtoLink;
   };
 
   return (
-    <section id="contact" className="contact section">
-      <div className="container" data-aos="fade">
-        <div className="row gy-5 gx-lg-5">
-          <div className="col-lg-4">
-            <div className="info">
-              <h3>{t('contact.getInTouch')}</h3>
-              <p>{t('contact.weLoveToHear')}</p>
+    <section id="contact" className="contact section vh-100 d-flex justify-content-center align-items-center">
+      <div className="contact-container" data-aos="fade">
+        <div className="info mb-4">
+          <h3>{t('contact.getInTouch')}</h3>
+          <p>{t('contact.weLoveToHear')}</p>
 
-              <div className="info-item d-flex">
-                <i className="bi bi-geo-alt flex-shrink-0"></i>
-                <div>
-                  <h4>{t('contact.location')}</h4>
-                  <p><a href="https://maps.app.goo.gl/PyKXt1yMdZcVTjXG6">AHN Kiropraktorerna, Söndrumsvägen 29, 302 37 Halmstad</a></p>
-                </div>
-              </div>
-
-              <div className="info-item d-flex">
-                <i className="bi bi-envelope flex-shrink-0"></i>
-                <div>
-                  <h4>{t('contact.email')}</h4>
-                  <p>info@ayurharmonyretreat.se</p>
-                </div>
-              </div>
-
-              <div className="info-item d-flex">
-                <i className="bi bi-phone flex-shrink-0"></i>
-                <div>
-                  <h4>{t('contact.call')}</h4>
-                  <p>+46 76 453 27 01</p>
-                </div>
-              </div>
+          <div className="info-item d-flex mb-3">
+            <i className="bi bi-geo-alt me-2"></i>
+            <div>
+              <h4>{t('contact.location')}</h4>
+              <p>
+                <a
+                  href="https://maps.app.goo.gl/PyKXt1yMdZcVTjXG6"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  AHN Kiropraktorerna, Söndrumsvägen 29, 302 37 Halmstad
+                </a>
+              </p>
             </div>
           </div>
 
-          <div className="col-lg-8">
-            <form ref={formRef} onSubmit={sendEmail} className="php-email-form">
-              <div className="row">
-                <div className="col-md-6 form-group">
-                  <input type="text" name="name" className="form-control" placeholder={t('contact.yourName')} required />
-                </div>
-                <div className="col-md-6 form-group mt-3 mt-md-0">
-                  <input type="email" className="form-control" name="email" placeholder={t('contact.yourEmail')} required />
-                </div>
-              </div>
-              <div className="form-group mt-3">
-                <input type="text" className="form-control" name="subject" placeholder={t('contact.subject')} required />
-              </div>
-              <div className="form-group mt-3">
-                <textarea className="form-control" name="message" placeholder={t('contact.message')} required></textarea>
-              </div>
-              <div className="text-center">
-                <button type="submit">{t('contact.bookNow')}</button>
-              </div>
-            </form>
+          <div className="info-item d-flex mb-3">
+            <i className="bi bi-envelope me-2"></i>
+            <div>
+              <h4>{t('contact.email')}</h4>
+              <p><a href="mailto:info@ayurharmonyretreat.se?subject=Inquiry">info@ayurharmonyretreat.se</a></p>
+            </div>
+          </div>
+
+          <div className="info-item d-flex mb-3">
+            <i className="bi bi-phone me-2"></i>
+            <div>
+              <h4>{t('contact.call')}</h4>
+              <p>
+                <a href="tel:+46764532701">{t('+46764532701')}</a>
+              </p>
+            </div>
+          </div>
+
+          <div className="text-center">
+            <a
+              href="mailto:info@ayurharmonyretreat.se?subject=Inquiry"
+              className="green-button btn mt-3"
+            >
+              {t('contact.bookNow')}
+            </a>
           </div>
         </div>
       </div>
